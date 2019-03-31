58c1a9a39f32fafe2c572a33f9e9f50741c59428
WillKKirby <49102068+WillKKirby@users.noreply.github.com>
2019-03-31 17:25:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame,random
+import pygame,random,time
 from pygame.locals import *
 
 # Start Stuff
@@ -28,9 +28,12 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 60
         self.rect.y = 310
-        self.x_change = 0
-        self.y_change = 0
+        self.x_change = 1
+        self.y_change = 1
         self.platform = platformGroup
+        self.platform2 = floorGroup
+        self.gravCounter = 0
+        self.gravCurve = [3.4113999999999995, 3.019000000000002, 2.626599999999998, 2.2341999999999995, 1.8417999999999992, 1.4494000000000042, 1.056999999999995, 0.6646000000000072, 0.27219999999999445, -0.12020000000000053, -0.5125999999999991, -0.9050000000000082, -1.2973999999999961, -1.6898000000000053, -2.082199999999993, -2.4746000000000095, -2.8670000000000044, -3.2594000000000065, -3.6517999999999944, -4.044200000000004]
         playerGroup.add(self)
 
     def update(self):
@@ -49,6 +52,30 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.rect.top = Q.rect.bottom
 
+        self.rect.x += self.x_change
+        block_hit_list = pygame.sprite.spritecollide(self,self.platform2,False)
+        for Q in block_hit_list:
+            if self.x_change > 0:
+                self.rect.right = Q.rect.left
+            else:
+                self.rect.left = Q.rect.right
+        self.rect.y += self.y_change
+        block_hit_list = pygame.sprite.spritecollide(self,self.platform2,False)
+        for Q in block_hit_list:
+            if self.y_change > 0:
+                self.rect.bottom = Q.rect.top
+            else:
+                self.rect.top = Q.rect.bottom
+
+        if self.rect.x > 800 or self.rect.x < 0:
+            print('Dead')
+            self.kill()
+            
+            
+    def Jump(self):
+
+        
+
 class PlatformBlock(pygame.sprite.Sprite):
     def __init__(self,x,y):
         super().__init__()
@@ -56,8 +83,10 @@ class PlatformBlock(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = [x,y]
         platformGroup.add(self)
-    def Move(self):
-        self.rect.x -= 1
+    def update(self):
+        self.rect.x -= 0.5
+        if self.rect.x > 800 or self.rect.x == 0:
+            self.kill()
 
 class floorBlock(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -81,11 +110,11 @@ def makeFloor():
     length = screenW//30
     startX = 0
     for Q in range(length+1):
-        floor = PlatformBlock(startX,370)
+        floor = floorBlock(startX,370)
         startX += 30
     startX = 0
     for Q in range(length+1):
-        floor = PlatformBlock(startX,340)
+        floor = floorBlock(startX,340)
         startX += 30
         
 def makePlatform():
@@ -101,8 +130,11 @@ def makePlatform():
 
 def main():
     genCycle = 1
+    makeObj = False
     run = True
-
+    jump = False
+    
+    
     makeFloor() # This makes the floor ... derrrr
 
     bg = pygame.image.load("images/bg.png")
@@ -115,14 +147,22 @@ def main():
                     run = False
                 if Event.type == pygame.KEYDOWN:
                     if Event.key == pygame.K_SPACE:
-                        Jump()
+                        jump = True
 
         ######################
-        # Obsticle Generation
+
+        # Logic Stuff
+
+        # Makes the platforms appear 
         genCycle -= 1
-        if genCycle == 0:
+        if genCycle == 0 and makeObj:
             makePlatform()
-            genCycle = 50
+            genCycle = 200
+
+        
+        # Jumpy Code
+
+            
         ######################
 
         # Drawing All the stuff on the screen 
@@ -132,8 +172,8 @@ def main():
 
         floorGroup.draw(gameDisplay)
         platformGroup.draw(gameDisplay)
-        for Q in platformGroup:
-            Q.Move()
+
+        platformGroup.update()
         playerGroup.draw(gameDisplay)
         playerGroup.update()
         pygame.display.update()
