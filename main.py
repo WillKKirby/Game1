f8be647bbae56ecabc904b321c6d80fa3a594aca
WillKKirby <49102068+WillKKirby@users.noreply.github.com>
2019-03-31 17:57:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,65 +28,51 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 60
         self.rect.y = 310
-        self.x_change = 1
-        self.y_change = 1
         self.platform = platformGroup
         self.platform2 = floorGroup
-        self.gravCounter = 0
+        self.gravCount = 0
         self.gravCurve = [3.4113999999999995, 3.019000000000002, 2.626599999999998, 2.2341999999999995, 1.8417999999999992, 1.4494000000000042, 1.056999999999995, 0.6646000000000072, 0.27219999999999445, -0.12020000000000053, -0.5125999999999991, -0.9050000000000082, -1.2973999999999961, -1.6898000000000053, -2.082199999999993, -2.4746000000000095, -2.8670000000000044, -3.2594000000000065, -3.6517999999999944, -4.044200000000004]
         playerGroup.add(self)
 
     def update(self):
-        self.rect.x += self.x_change
-        block_hit_list = pygame.sprite.spritecollide(self,self.platform,False)
-        for Q in block_hit_list:
-            if self.x_change > 0:
-                self.rect.right = Q.rect.left
-            else:
-                self.rect.left = Q.rect.right
-        self.rect.y += self.y_change
-        block_hit_list = pygame.sprite.spritecollide(self,self.platform,False)
-        for Q in block_hit_list:
-            if self.y_change > 0:
-                self.rect.bottom = Q.rect.top
-            else:
-                self.rect.top = Q.rect.bottom
-
-        self.rect.x += self.x_change
-        block_hit_list = pygame.sprite.spritecollide(self,self.platform2,False)
-        for Q in block_hit_list:
-            if self.x_change > 0:
-                self.rect.right = Q.rect.left
-            else:
-                self.rect.left = Q.rect.right
-        self.rect.y += self.y_change
-        block_hit_list = pygame.sprite.spritecollide(self,self.platform2,False)
-        for Q in block_hit_list:
-            if self.y_change > 0:
-                self.rect.bottom = Q.rect.top
-            else:
-                self.rect.top = Q.rect.bottom
 
         if self.rect.x > 800 or self.rect.x < 0:
             print('Dead')
             self.kill()
-            
-            
-    def Jump(self):
-
-        
 
 class PlatformBlock(pygame.sprite.Sprite):
-    def __init__(self,x,y):
+    def __init__(self,height,x,y):
         super().__init__()
         self.image = pygame.image.load("images/platform.png")
+        self.image = pygame.transform.scale(self.image,[30,(30*height)])
         self.rect = self.image.get_rect()
-        self.rect.topleft = [x,y]
+        self.rect.topleft = [x,y-(30*height)]
+        self.x_change = 1
+        self.y_change = 0
+        self.platform = playerGroup
         platformGroup.add(self)
+    
     def update(self):
-        self.rect.x -= 0.5
         if self.rect.x > 800 or self.rect.x == 0:
             self.kill()
+
+        self.rect.x -= self.x_change
+        block_hit_list = pygame.sprite.spritecollide(self,self.platform,False)
+        for Q in block_hit_list:
+            player.rect.x -= self.x_change
+            if self.x_change < 0:
+                self.rect.right = Q.rect.left
+            else:
+                self.rect.left = Q.rect.right
+                
+        self.rect.y - self.y_change
+        block_hit_list = pygame.sprite.spritecollide(self,self.platform,False)
+        for Q in block_hit_list:
+            if self.y_change > 0:
+                self.rect.bottom = Q.rect.top
+            else:
+                self.rect.top = Q.rect.bottom
+        
 
 class floorBlock(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -118,19 +104,16 @@ def makeFloor():
         startX += 30
         
 def makePlatform():
-    length = random.randint(2,10)
     height = random.randint(1,4)
-    StartY = 340
-    for Q in range(height):
-        platfrom = PlatformBlock(730,StartY)
-        StartY -= 30
+    platfrom = PlatformBlock(height,730,340)
+
 ######################
 
 # Main Loop
 
 def main():
     genCycle = 1
-    makeObj = False
+    makeObj = True
     run = True
     jump = False
     
@@ -161,6 +144,14 @@ def main():
 
         
         # Jumpy Code
+        if jump == True:
+            player.rect.y -= player.gravCurve[player.gravCount]
+            player.gravCount += 1
+            if player.gravCount == len(player.gravCurve):
+                player.gravCount = 0
+                jump = False
+            if player.rect.y > 310:
+                player.rect.y = 310
 
             
         ######################
