2ff85181b49a395f6d5327ec80833a6866b783ff
WillKKirby <49102068+WillKKirby@users.noreply.github.com>
2019-03-31 18:21:55 +0100

    OMG LOOK AT THIS
    There is a few things still to change but we can start on the design of the game like how it will play and look soon

--- main.py
+++ main.py
@@ -23,30 +23,28 @@ floorGroup = pygame.sprite.Group()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("images/playerImg.png")
+        self.image = pygame.image.load("images/playerImg.png") # image loading
         self.image = pygame.transform.scale(self.image,[30,30])
         self.rect = self.image.get_rect()
-        self.rect.x = 60
-        self.rect.y = 310
-        self.platform = platformGroup
-        self.platform2 = floorGroup
-        self.gravCount = 0
+        self.rect.x = 100 
+        self.rect.y = 310 # These are the starting positions 
+        self.gravCount = 0 # This is the grav jumping curve
         self.gravCurve = [1.7547500000000005, 1.656649999999999, 1.5585500000000003, 1.4604499999999998, 1.362350000000001, 1.264249999999997, 1.1661500000000036, 1.0680499999999977, 0.9699499999999972, 0.871850000000002, 0.7737499999999997, 0.6756500000000045, 0.5775499999999951, 0.47945000000000704, 0.38134999999999764, 0.28324999999999534, 0.1851500000000037, 0.08704999999999785, -0.01104999999999734, -0.10915000000000319, -0.20725000000000193, -0.3053500000000007, -0.403450000000003, -0.5015500000000017, -0.599650000000004, -0.6977499999999992, -0.7958499999999944, -0.8939500000000038, -0.9920500000000061, -1.0901500000000084, -1.1882500000000036, -1.2863500000000059, -1.3844500000000082, -1.482549999999975, -1.5806500000000128, -1.678750000000008, -1.776849999999996, -1.8749499999999983, -1.9730500000000006]
-        playerGroup.add(self)
+        playerGroup.add(self) # Obviously adding self to group
 
     def update(self):
-
         if self.rect.x > 800 or self.rect.x < 0:
-            print('Dead')
+            print('Dead') # Check on screen 
             self.kill()
 
 class PlatformBlock(pygame.sprite.Sprite):
-    def __init__(self,height,x,y):
+    def __init__(self,x,y):
         super().__init__()
+        self.height = random.randint(1,4)
         self.image = pygame.image.load("images/platform.png")
-        self.image = pygame.transform.scale(self.image,[30,(30*height)])
+        self.image = pygame.transform.scale(self.image,[30,(30*self.height)])
         self.rect = self.image.get_rect()
-        self.rect.topleft = [x,y-(30*height)]
+        self.rect.topleft = [x,y-(30*self.height)]
         self.x_change = 1.5
         self.y_change = 0
         self.platform = playerGroup
@@ -64,17 +62,19 @@ class PlatformBlock(pygame.sprite.Sprite):
                 self.rect.right = Q.rect.left
             else:
                 self.rect.left = Q.rect.right
-                
-        self.rect.y - self.y_change
+
+        self.y_change = 1 # THIS IS STILL BROKEN - YOU CAN'T LAND ON TOP OF THEM 
+        self.rect.y -= self.y_change
         block_hit_list = pygame.sprite.spritecollide(self,self.platform,False)
         for Q in block_hit_list:
             if self.y_change > 0:
                 self.rect.bottom = Q.rect.top
             else:
                 self.rect.top = Q.rect.bottom
+        self.rect.y += 1
         
 
-class floorBlock(pygame.sprite.Sprite):
+class floorBlock(pygame.sprite.Sprite): # Floor 
     def __init__(self,x,y):
         super().__init__()
         self.image = pygame.image.load("images/platform.png")
@@ -100,12 +100,8 @@ def makeFloor():
         startX += 30
     startX = 0
     for Q in range(length+1):
-        floor = floorBlock(startX,340)
+        floor = floorBlock(startX,340) # I'm lazy, there are 2 so it is ticker 
         startX += 30
-        
-def makePlatform():
-    height = random.randint(1,4)
-    platfrom = PlatformBlock(height,730,340)
 
 ######################
 
@@ -141,13 +137,14 @@ def main():
         # Makes the platforms appear 
         genCycle -= 1
         if genCycle == 0 and makeObj:
-            makePlatform()
+            platfrom = PlatformBlock(730,340)
             genCycle = 200
 
         
         # Jumpy Code
         if jump == True:
             player.rect.y -= player.gravCurve[player.gravCount]*10
+            player.rect.x += 1
             player.gravCount += 1
             if player.gravCount == len(player.gravCurve):
                 player.gravCount = 0
@@ -173,11 +170,9 @@ def main():
 
         clock.tick(fps)
 
-    gameDisplay.close()
-
 main()
 
-
+pygame.display.quit()
 
 
 
